cd9996c5f6a0099bf05fd4e37e1d755f8fd56458
Eloi RM <eloimailmaynad@gmail.com>
2024-11-28 12:20:55 +0100

    affichage de 4 cartes

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-const grimoireButton  = document.querySelector(".grimoire-button")
 const grimoireImage = document.querySelector(".grimoire-image")
 
-grimoireButton.addEventListener('click', ()=>{ //ouvre notre grimoire
-    grimoireButton.classList.add("open")
-    grimoireImage.src = "grimoire_ouvert.jpg"
-    grimoireButton.disabled = true
-})
+//////////Maison de Grace
+//
+///////////////////////
+
+const leftSection = document.querySelector(".left-section")
+
+async function getCardsList(){
+    const response = await fetch("https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Ablb&unique=prints")
+    const json = await response.json()
+    console.log(json)
+    return json
+}
+
+async function game() {
+const cardList = await getCardsList()
+displayCard(cardList)
+}
+
+function displayCard(myList) {
+console.log(myList.data[0].name)
+console.log(myList.data[0].image_uris.small)
+for (i = 0; i < 4; i++){
+    leftSection.innerHTML += `<img src="${myList.data[i].image_uris.small}">
+                             <div>${myList.data[i].name}<div/><br>`
+}
+}
+
+//for 0 a 1 en j
+// for de 0 8 en i
+// j ++
+
+game()
+
+//une conteneur avec 2 div dedans par carte
+
+
+
