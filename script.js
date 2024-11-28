5a586005b231b58ad614703f2e211eefc1c87e64
Sebus83 <sebastien.herbiet@gmail.com>
2024-11-28 16:42:36 +0100

    texte ok page gauche

--- script.js
+++ script.js
@@ -46,8 +46,10 @@ async function displayCard(info) {
         
 
         const dataContainer = document.createElement("p");
-        dataContainer.innerHTML +=`<article><h2>${item.sign}</h2><p>${item.description}</p></article>`;
-
+        dataContainer.innerHTML =`<article><h1>${infoCard.name}</h1></br><p><u><strong>Mana</u></strong></br><p>${infoCard.mana_cost}</p></br></p><p><u><strong>Type</u></strong></br><p>${infoCard.type_line}</p></br><p><u><strong>Effet</u></strong></br><p>${infoCard.oracle_text}</p></br><p><u><strong>Att/Def</u></strong></br><p>${infoCard.power||"-"} / ${infoCard.toughness||"-"}</p></br><p><u><strong>Rareté</u></strong></br><p>${infoCard.rarity}</p></br><p><u><strong>Collection</u></strong></br><p>${infoCard.set_name}</p></br><p><u><strong>Couleur</u></strong></br><p>${infoCard.colors}</p></br><p><u><strong>Citation</u></strong></br><p>${infoCard.flavor_text||""}</p></br><p><u><strong>Artiste / Année</u></strong></br><p>${infoCard.artist||""} / ${infoCard.frame||""}</p></article>`;
+        dataContainer.classList.add("card-normal-text")
+        rightSection.appendChild(dataContainer)
+        
 
     } catch (error) {
         console.error("Carte non trouvée", error);
