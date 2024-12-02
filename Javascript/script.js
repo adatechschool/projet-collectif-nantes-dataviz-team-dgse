cd3fb7f3dd3c73d77f7e105d8bfe3bd68a63279d
Sebus83 <sebastien.herbiet@gmail.com>
2024-12-02 15:57:52 +0100

    pas d'accueil gauche

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,10 +3,27 @@ import { chooseSet, displayCardList, indexPage, cardsList } from "./cardsList.mj
 const grimoireImage = document.querySelector(".grimoire-image")
 const leftSection = document.querySelector(".left-section")
 const rightSection = document.querySelector(".right-section")
+const rightSectionhome = document.querySelector(".right-section-homepage")
 const nextPageButton = document.querySelector(".next-page-button")
 const previousPageButton = document.querySelector(".previous-page-button")
+const homePageButton = document.querySelector('.homepage-button')
+const grimoireContainer = document.querySelector('.grimoire-container')
 
-chooseSet()
+
+homePageButton.addEventListener("click", ()=>
+    {
+    grimoireContainer.style.display = 'inline-block';
+    homePageButton.style.display = 'none'
+    nextPageButton.style.display = "none"
+    previousPageButton.style.display = "none" 
+    rightSection.innerHTML = `<article><h1>Mode d'emploi</h1> <p>Afin de naviguer au mieux, vous pouvez utiliser les boutons "Set", "Couleur" et "Rareté" ci dessus.</p>
+                                <p>le moteur de recherche "Boule de cristal" en haut à droite peut vous aider</p></article>`;
+    leftSection.innerHTML = `<article><h1>Bienvenue dans le monde magique de </h1><h2>MAGIC THE GATHERING</h2></article>`;
+    
+                               
+    })
+
+//chooseSet()
 
 nextPageButton.addEventListener("click", ()=> { // ajoute un écouteur d'évenements "clique" sur bouton suivant
     indexPage.index ++
@@ -18,5 +35,5 @@ previousPageButton.addEventListener("click", ()=> { // ajoute un écouteur d'év
 })
 
 export {
-    grimoireImage, leftSection, rightSection, nextPageButton, previousPageButton
+    grimoireImage, leftSection, rightSection, nextPageButton, previousPageButton,homePageButton, grimoireContainer
 }
