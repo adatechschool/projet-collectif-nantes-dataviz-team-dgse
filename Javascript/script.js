e19187f96842fc2043ead91f36139081e8c92be2
Grace Judic <gracejudic@MacBook-de-Grace.local>
2024-12-02 17:17:46 +0100

    ajout myButtons

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,12 +1,14 @@
 import { doCardsList, displayCardList, indexPage, cardsList } from "./cardsList.mjs"
+import { displayCard } from "./singleCard.mjs"
 
 const grimoireImage = document.querySelector(".grimoire-image")
 const leftSection = document.querySelector(".left-section")
 const rightSection = document.querySelector(".right-section")
 const nextPageButton = document.querySelector(".next-page-button")
 const previousPageButton = document.querySelector(".previous-page-button")
+const randomButton = document.querySelector(".random-button")
 
-doCardsList("https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Amid&unique=prints")
+doCardsList("https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Ablb&unique=prints")
 
 nextPageButton.addEventListener("click", ()=> { // ajoute un écouteur d'évenements "clique" sur bouton suivant
     indexPage.index ++
@@ -15,6 +17,10 @@ nextPageButton.addEventListener("click", ()=> { // ajoute un écouteur d'évenem
 previousPageButton.addEventListener("click", ()=> { // ajoute un écouteur d'évenements "clique" sur bouton précédent
     indexPage.index --
     displayCardList(cardsList, indexPage.index)
+})
+
+randomButton.addEventListener("click", ()=> {
+    displayCard("random")
 })
 
 export {
