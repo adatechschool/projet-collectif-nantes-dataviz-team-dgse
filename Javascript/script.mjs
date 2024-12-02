2c5c0ed417ae6661d468e9a6be90cced96f835ac
Sebus83 <sebastien.herbiet@gmail.com>
2024-12-02 15:57:52 +0100

    pas d'accueil gauche

--- Javascript/script.mjs
+++ Javascript/script.mjs
@@ -4,8 +4,27 @@ import { doSetsList } from "./setsList.mjs"
 const grimoireImage = document.querySelector(".grimoire-image")
 const leftSection = document.querySelector(".left-section")
 const rightSection = document.querySelector(".right-section")
+const rightSectionhome = document.querySelector(".right-section-homepage")
 const nextPageButton = document.querySelector(".next-page-button")
 const previousPageButton = document.querySelector(".previous-page-button")
+const homePageButton = document.querySelector('.homepage-button')
+const grimoireContainer = document.querySelector('.grimoire-container')
+
+
+homePageButton.addEventListener("click", ()=>
+    {
+    grimoireContainer.style.display = 'inline-block';
+    homePageButton.style.display = 'none'
+    nextPageButton.style.display = "none"
+    previousPageButton.style.display = "none" 
+    rightSection.innerHTML = `<article><h1>Mode d'emploi</h1> <p>Afin de naviguer au mieux, vous pouvez utiliser les boutons "Set", "Couleur" et "Rareté" ci dessus.</p>
+                                <p>le moteur de recherche "Boule de cristal" en haut à droite peut vous aider</p></article>`;
+    leftSection.innerHTML = `<article><h1>Bienvenue dans le monde magique de </h1><h2>MAGIC THE GATHERING</h2></article>`;
+    
+                               
+    })
+
+//chooseSet()
 
 // doCardsList("https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Amid&unique=prints")
 
@@ -23,5 +42,7 @@ previousPageButton.addEventListener("click", ()=> { // ajoute un écouteur d'év
 })
 
 export {
+    grimoireImage, leftSection, rightSection, nextPageButton, previousPageButton,homePageButton, grimoireContainer
+}
     grimoireImage, leftSection, rightSection, nextPageButton, previousPageButton
 }   
