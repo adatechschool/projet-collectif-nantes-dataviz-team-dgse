efc3bc570b78eb950933c73a9a794e2b0393a87a
Sebus83 <sebastien.herbiet@gmail.com>
2024-12-03 16:46:40 +0100

    maj dev search

--- Javascript/script.mjs
+++ Javascript/script.mjs
@@ -19,12 +19,8 @@ openBookButton.addEventListener("click", ()=>
     openBookButton.style.display = 'none'
     nextPageButton.style.display = "none"
     previousPageButton.style.display = "none" 
-    homePageButton.style.display = 'none'
-    // nextPageButton.style.display = "none"
-    // previousPageButton.style.display = "none" 
     rightSection.innerHTML = `<article><h1>Mode d'emploi</h1> <p>Afin de naviguer au mieux, vous pouvez utiliser les boutons "Set", "Couleur" et "Rareté" ci dessus.</p>
                                 <p>le moteur de recherche "Boule de cristal" en haut à droite peut vous aider</p></article>`;
-    leftSection.innerHTML = `<article><h1>Bienvenue dans le monde magique de </h1><h2>MAGIC THE GATHERING</h2></article>`;                             
     leftSection.innerHTML = `<article><h1>Bienvenue dans le monde magique de </h1><h2>MAGIC THE GATHERING</h2></article>`;
     doSetsList()                         
     })
@@ -32,6 +28,28 @@ openBookButton.addEventListener("click", ()=>
 //chooseSet()
 
 // doCardsList("https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Amid&unique=prints")
+
+// Validation en appuyant sur Entrée
+searchInput.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        const searchTerm = searchInput.value
+        performSearch(searchTerm);
+        
+    }
+});
+
+function performSearch(myString) {
+    
+    nextPageButton.style.display = "flex"
+    previousPageButton.style.display = "flex"
+    if (myString === '') {
+        resultMessage.textContent = 'Veuillez saisir un terme de recherche';
+        resultMessage.style.color = 'red';
+        return;
+    }
+    const url = `https://api.scryfall.com/cards/search?q=${myString}`
+        doCardsList(url);   
+}
 
 
 
