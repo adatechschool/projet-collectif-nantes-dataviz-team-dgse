c1522a0c9af33decbd7d85e830082e290a809835
Grace Judic <gracejudic@MacBook-de-Grace.local>
2024-12-04 10:09:34 +0100

    boutons last check

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,6 @@
 import { doCardsList, displayCardList, indexPage, cardsList } from "./cardsList.mjs"
 import { displayCard } from "./singleCard.mjs"
+import { displayArtists, artistList } from "./displayArtists.mjs"
 
 const grimoireImage = document.querySelector(".grimoire-image")
 const leftSection = document.querySelector(".left-section")
@@ -10,11 +11,9 @@ const randomButton = document.querySelector(".random-button")
 const setButton = document.querySelector(".set-button")
 const rareButton = document.querySelector(".rare-button")
 const colorButton = document.querySelector(".color-button")
-const setTypeButton = document.querySelector(".set-type-button")
 const authorButton = document.querySelector(".author-button")
 const homeButton = document.querySelector(".home-button")
 
-const artistList = []
 
 doCardsList("https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Ablb&unique=prints")
 
@@ -35,42 +34,7 @@ authorButton.addEventListener("click",()=> {
     displayArtists(artistList,indexPage.index)
 })
 
-async function displayArtists(myList, myPage) {
-    leftSection.innerHTML = ""
-    rightSection.innerHTML = ""
-    myPage = myPage * 12
-
-
-    const reponse = await fetch("https://api.scryfall.com/catalog/artist-names")
-    const json = await reponse.json()
-
-    for (const artist of json.data) {
-        myList.push(artist) 
-    }
-
-    for (let i = myPage; i < myPage + 6; i++) {
-
-        if (i < myList.length) {
-            const nameArtist = document.createElement("p")
-            nameArtist.innerText = myList[i]
-            nameArtist.classList.add("name-artist")
-            leftSection.appendChild(nameArtist)
-        }
-    }
-    
-    for (let i = myPage; i < myPage + 12; i++){
-        if (i < myList.length){   
-            const nameArtist = document.createElement("p")
-            nameArtist.innerText = myList[i]
-            nameArtist.classList.add("name-artist")
-            rightSection.appendChild(nameArtist)
-        }
-    }    
-    console.log(artistList)
-}
-
-
 
 export {
-    grimoireImage, leftSection, rightSection, nextPageButton, previousPageButton
+    grimoireImage, leftSection, rightSection, nextPageButton, previousPageButton, 
 }   
